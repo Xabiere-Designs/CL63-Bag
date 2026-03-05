7a2929a7dd947ce140d42aef34ec44ab08a1358d
xabiere-designs <xabieredesigns@gmail.com>
2026-03-05 10:33:48 -0500

    code sweep

--- Infra/terraform/module/addons-helm/variables.tf
+++ Infra/terraform/module/addons-helm/variables.tf
@@ -1,8 +1,17 @@
 variable "cluster_name" { type = string }
 
-variable "enable_ingress_nginx" { type = bool default = true }
-variable "enable_istio"         { type = bool default = false }
-variable "enable_kps"           { type = bool default = true } # kube-prometheus-stack
+variable "enable_ingress_nginx" {
+  type    = bool
+  default = true
+}
+variable "enable_istio" {
+  type    = bool
+  default = false
+}
+variable "enable_kps" {
+  type    = bool
+  default = true
+} # kube-prometheus-stack
 
 variable "tags" {
   type    = map(string)
