2e01c3e2f0fecd0039ee881da9ed10b821c1ded3
xabiere-designs <xabieredesigns@gmail.com>
2026-03-05 07:38:23 -0500

    dev variables file patch

--- Infra/terraform/module/eks/variables.tf
+++ Infra/terraform/module/eks/variables.tf
@@ -12,9 +12,20 @@ variable "node_instance_types" {
   default = ["t3.large"]
 }
 
-variable "desired_size" { type = number default = 2 }
-variable "min_size"     { type = number default = 1 }
-variable "max_size"     { type = number default = 4 }
+variable "desired_size" {
+  type    = number
+  default = 2
+}
+
+variable "min_size" {
+  type    = number
+  default = 1
+}
+
+variable "max_size" {
+  type    = number
+  default = 4
+}
 
 variable "tags" {
   type    = map(string)
