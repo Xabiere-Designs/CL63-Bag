dfbdf3f39d5d9276918293f443241fab279810e2
xabiere-designs <xabieredesigns@gmail.com>
2026-03-05 09:37:46 -0500

    envs variables refactoring

--- Infra/terraform/envs/prod/variables.tf
+++ Infra/terraform/envs/prod/variables.tf
@@ -1,8 +1,27 @@
-variable "region"  { type = string default = "us-east-1" }
-variable "org"     { type = string default = "xabiere" }
-variable "project" { type = string default = "platform" }
-variable "env"     { type = string default = "prod" }
-variable "owner"   { type = string default = "Corey L. Ducre" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "org" {
+  type    = string
+  default = "xabiere"
+}
+
+variable "project" {
+  type    = string
+  default = "platform"
+}
+
+variable "env" {
+  type    = string
+  default = "prod"
+}
+
+variable "owner" {
+  type    = string
+  default = "Corey L. Ducre"
+}
 
 variable "vpc_cidr" { type = string }
 variable "azs"      { type = list(string) }
